5b2fb5e38e2698b78c94359900a468c2c87a6abc
carrer55 <carrerleger.55@gmail.com>
2025-08-30 18:12:00 +0900

    Remove authentication debug component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import AuthWrapper from './components/AuthWrapper';
 import { UserProfileProvider } from './components/UserProfileProvider';
-import { AuthDebug } from './components/AuthDebug';
 import './index.css';
 
 function App() {
   return (
     <UserProfileProvider>
       <AuthWrapper />
-      <AuthDebug />
     </UserProfileProvider>
   );
 }
